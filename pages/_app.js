25029bf8b1c250ff59cd3c9bfcf2139758728590
santerisarle <santeri.sarle@gmail.com>
2024-03-03 08:37:24 +0800

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -31,6 +31,38 @@ function MyApp({ Component, pageProps }) {
           site: "@dafacto",
           cardType: "summary_large_image",
         }}
+        additionalLinkTags={[
+          {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: "/apple-touch-icon.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: "/favicon-32x32.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: "/favicon-16x16.png",
+          },
+          {
+            rel: "icon",
+            href: "/favicon.ico",
+          },
+          {
+            rel: "mask-icon",
+            href: "/safari-pinned-tab.svg",
+            color: "#0f172a",
+          },
+          {
+            rel: "manifest",
+            href: "/site.webmanifest",
+          },
+        ]}
       />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
